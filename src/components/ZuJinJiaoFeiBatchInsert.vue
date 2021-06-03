b5aad4f428169ec67f3d543bd7c6eb0fb69bb73c
Ouka <729529622@qq.com>
2021-06-03 20:07:53 +0800

    更正加载vue组件问题。

--- src/components/ZuJinJiaoFeiBatchInsert.vue
+++ src/components/ZuJinJiaoFeiBatchInsert.vue
@@ -2,7 +2,7 @@
   <div>
     <button class="btn btn-primary" @click="dialogVisible = true">{{ msg }}</button>
 
-    <el-dialog v-model="dialogVisible" width="30%" :fullscreen="dialogFullScreen" @closed="closedHandler"
+    <el-dialog v-model="dialogVisible" width="60%" :fullscreen="dialogFullScreen" @closed="closedHandler"
                :append-to-body="true">
       <template #title>
         <div class="dialog-title-div">{{ msg }}</div>
@@ -19,9 +19,9 @@
       </el-steps>
       <el-card shadow="never" v-show="stepCurrent === 0">
         <el-row type="flex" justify="center">
-          <el-col :span="12"><input type="file"></el-col>
+          <el-col :span="12"><input type="file" @change="excelFileChangeHandler"></el-col>
           <el-col :span="4">
-            <el-button type="primary" @click="doCard1">加载</el-button>
+            <el-button type="primary" @click="loadDataHanler">加载</el-button>
           </el-col>
         </el-row>
       </el-card>
@@ -43,9 +43,9 @@
         </el-row>
       </el-card>
       <el-card shadow="never" v-show="stepCurrent === 2">
-        <el-row>
-          <input type="file">
-          <el-button type="primary" @click="doCard3">加载3</el-button>
+        <el-row :gutter="10" type="flex" justify="center">
+          <el-col :span="12"><input type="file"></el-col>
+            <el-col :span="4"><el-button type="primary" @click="doCard3">加载3</el-button></el-col>
         </el-row>
       </el-card>
       <el-card shadow="never" :body-style="{padding:'2px'}" :v-show="showResult">
@@ -68,6 +68,7 @@
 
 <script lang="ts">
 import {reactive, ref} from "vue";
+import { excel } from "../utils";
 
 interface Option {
   label: string,
@@ -91,6 +92,17 @@ export default {
     const resultList = ref<string[]>([])
     const selectedValue = ref<string>('')
     const options = ref<Option[]>([])
+    const excelFile = ref<File>()
+
+    // webSQL
+    const _webSql = window.openDatabase('threeFeiWebSQL', '1.0', '', 2 * 1024 * 1024, () => {
+      console.debug('webSQL loaded.')
+    })
+    _webSql.transaction(function (tx) {
+      tx.executeSql('DROP TABLE IF EXISTS zuJin_guHuaLuRu_insert')
+    })
+
+    const webSql = reactive(_webSql)
 
     // 必须返回模块中才能够使
     return {
@@ -102,13 +114,15 @@ export default {
       resultList,
       selectedValue,
       options,
-      dialogReset
+      dialogReset,
+      webSql,
+      excelFile
     }
   },
   methods: {
-    async doCard1() {
-      this.stepCurrent++
-    },
+    // async doCard1() {
+    //   this.stepCurrent++
+    // },
     async doCard2() {
       this.stepCurrent++
     },
@@ -130,24 +144,47 @@ export default {
     async hideHandler() {
       this.dialogReset = false
       this.dialogVisible = false
+    },
+    async excelFileChangeHandler(e: Event) {
+      //console.info(e)
+      const target = e.target as HTMLInputElement
+      this.excelFile = target.files.item(0)
+    },
+    async loadDataHanler() {
+      if (!this.excelFile) {
+        window.alert('请选择文件。')
+        return
+      }
+
+      this.options = []
+      let wb = await excel.xlsx.load(await this.excelFile.arrayBuffer())
+      wb.eachSheet(async (ws, id) => {
+        console.info(`eachSheet {label: ${ws.name}, value: ${id}}`)
+        this.options.push({label: ws.name, value: id} as Option)
+        // threeFei.sheetsValues.push(await changeRowsToDict(ws))
+      })
+      // console.debug(threeFei.sheetIndex)
+      // console.debug(threeFei.sheetsValues)
+      // let u = $('#zuJin_guHuaLuRu_insert_sheetSelector .panel-body .form-group:first')
+      // u.empty()
+      // threeFei.sheetIndex.forEach((val, index) => {
+      //   console.debug(`value: ${val}, index: ${index}`)
+      //   u.append(`<label class="btn btn-default"><input type="radio" name="selectedSheet" value="${index}">${val}</label>`)
+      // })
+      // $('#zuJin_guHuaLuRu_insert_sheetSelector').collapse('show')
+      this.stepCurrent++
     }
   },
   mounted() {
     for (let i = 0; i < 10; i++) {
       const v = Math.random().toString()
       this.resultList.push(`<span style="color: red">${v}</span>`)
-
-      this.options.push({label: 'opt' + i, value: i} as Option)
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.result-div {
-  max-height: 100px;
-  overflow: scroll;
-}
 
 .dialog-title-div {
   float: left;
